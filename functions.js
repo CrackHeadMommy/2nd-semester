a451a03c81f5d1691fbe7464e253752f79db9a3f
CrackHeadMommy <112924655+CrackHeadMommy@users.noreply.github.com>
2023-01-11 09:41:09 +0200

    fgv
    dfuyh

--- functions.js
+++ functions.js
@@ -1,5 +1,43 @@
+///Jānolasa ievadītais
+///Jāizvēlas skaitlis (datoram)
+///Jāpārbauda ievadītais
+///Jāievada atbilde
+
+let pcnummber = 10;
+
+
 function readinput(){
-    let inserted=document.getElementById("guess").value
-    
-    console.log(inserted)
+    let inserted=document.getElementById("guess").value;
+    console.log(inserted);
+    return inserted;
 } 
+
+
+function compareNummbers(humanNummber){
+    if (humanNummber>pcnummber){
+        return "PC nummber is smaller"
+    };
+    if (humanNummber<pcnummber){
+        return "PC nummber is bigger"
+    };
+    return "Winner!"
+}
+
+
+
+
+
+function check(){
+    console.log("check", pcnummber)
+    let guess2 = readinput();
+    let answer = compareNummbers(guess2);
+    insertanswer(answer)
+}
+function changeNummber(){
+    pcnummber = Math.round(Math.random() * 100);
+    console.log(pcnummber)
+}
+
+function answer(answer){
+    document.getElementById(answer).innerHTML=answer;
+}
